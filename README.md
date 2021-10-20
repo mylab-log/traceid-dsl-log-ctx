4de061df5f18c9d519602dd20fa167afd231acf5
Andrey O <ozzy.ext@gmail.com>
2021-10-20 03:27:18 +0300

    Add iniital project

---
+++ README.md
@@ -0,0 +1,5 @@
+# MyLab.Log.Tracing
+[![NuGet Version and Downloads count](https://buildstats.info/nuget/MyLab.Log.Tracing)
+
+Добавляет `OpenTelemetry` идетификатор трассировки в каждую запись лога.
+
---
+++ src/MyLab.Log.Tracing/TraceIdDslLogContextApplier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MyLab.Log.Dsl;
+
+namespace MyLab.Log.Tracing
+{
+    class TraceIdDslLogContextApplier : IDslLogContextApplier
+    {
+        public DslExpression Apply(DslExpression dslExpression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
